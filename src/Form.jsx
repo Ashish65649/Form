5daebdf75ad8b8cc4b352570ca25f4987c9e0011
Ashish Ranjan Kumar <imashish2589@gmail.com>
2021-07-21 15:07:58 +0530

    Initial Commit

---
+++ src/Form.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./Form.css";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function sendDataToServer(obj) {
+    fetch("http://localhost:9090/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error.message));
+  }
+
+  function submitForm(event) {
+    event.preventDefault();
+    let obj = {
+      userName: name,
+      userEmail: email,
+      userPassword: password,
+    };
+    sendDataToServer(obj);
+  }
+
+  return (
+    <React.Fragment>
+      <div className="wrapper">
+        <p>Application Form</p>
+        <form onSubmit={submitForm}>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input
+              onInput={(event) => setName(event.target.value)}
+              className="form-control"
+              type="text"
+              id="name"
+              value={name}
+              placeholder="Enter your name: "
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              onInput={(event) => setEmail(event.target.value)}
+              className="form-control"
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Enter your email: "
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              onInput={(event) => setPassword(event.target.value)}
+              className="form-control"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Enter your password: "
+            />
+          </div>
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Form;
